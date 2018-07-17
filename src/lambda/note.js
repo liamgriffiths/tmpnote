9ea878a5fcb89c2a7bd273cfe712d791146f620c
Liam Griffiths <liamgriffiths@gmail.com>
2018-07-16 22:12:05 -0400

    add some random console.logs

--- src/lambda/note.js
+++ src/lambda/note.js
@@ -10,22 +10,26 @@ const key = (uuid) => (
 )
 
 const create = async ({ note }) => {
+  console.log('create', Date.now())
   const id = uuid()
   await redis.set(key(id), note, 'NX', 'EX', ONE_WEEK)
   return { id }
 }
 
 const destroy = async ({ id }) => {
+  console.log('destroy', Date.now())
   await redis.del(key(id))
   return { status: 'ok' }
 }
 
 const read = async ({ id }) => {
+  console.log('read', Date.now())
   const [[_1, note], _2] = await redis.multi().get(key(id)).del(key(id)).exec()
   return { note }
 }
 
 const handlePost = async (event) => {
+  console.log('handlePost', Date.now())
   try {
     const { action, payload } = JSON.parse(event.body)
     switch (action) {
@@ -44,15 +48,18 @@ const handlePost = async (event) => {
 }
 
 export const handler = (event, context, callback) => {
+  console.log('handler', Date.now())
   if (event.httpMethod === 'POST') {
     handlePost(event)
       .then((res) => {
+        console.log('success', Date.now())
         callback(null, {
           statusCode: 200,
           body: JSON.stringify(res)
         })
       })
       .catch((err) => {
+        console.log('error', Date.now())
         callback(null, {
           statusCode: 500,
           body: 'Something went wrong.',
