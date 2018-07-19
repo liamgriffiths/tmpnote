c2330b8742fc13bd5c7721128a48148c74319885
Liam Griffiths <liamgriffiths@gmail.com>
2018-07-18 21:31:51 -0400

    add react-snap

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { hydrate, render } from 'react-dom'
+import { loadComponents, getState } from 'loadable-components'
 import './index.css'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+window.snapSaveState = () => getState()
+
+const rootElement = document.getElementById('root')
+if (rootElement.hasChildNodes()) {
+  loadComponents().then(() => {
+    hydrate(<App />, rootElement)
+  });
+} else {
+  render(<App />, rootElement)
+}
+
 registerServiceWorker()
