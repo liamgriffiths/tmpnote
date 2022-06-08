72305a2668e3921733560fbc42417a478400972e
Liam Griffiths <liamgriffiths@gmail.com>
2022-06-08 09:20:48 -0400

    Add a show page

---
+++ pages/show.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next';
+// import Head from 'next/head'
+// import Image from 'next/image'
+// import styles from '../styles/Home.module.css'
+
+const Show: NextPage = () => {
+  return (
+    <pre>hello</pre>
+  );
+};
+
+export default Show;
