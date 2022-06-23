07496161d3bd8a7dd6c191fb3ed82925d20b17b7
Liam Griffiths <liamgriffiths@gmail.com>
2022-06-22 23:01:50 -0400

    Use apos entity

--- components/GoneForever.tsx
+++ components/GoneForever.tsx
@@ -5,7 +5,7 @@ const GoneForever = () => {
     <div className="flex justify-center">
     <div className="flex flex-col items-center ring-2 ring-red-500 ring-offset-2 ring-offset-red-300 bg-red-50 p-4 m-4 rounded fg-black text-xl drop-shadow-2xl w-11/12 max-w-xl">
       <h1 className="font-bold text-3xl m-2">Destroyed</h1>
-      <h2 className="m-4">It's gone forever now</h2>
+      <h2 className="m-4">It&apos;s gone forever now</h2>
     </div>
     </div>
   );
