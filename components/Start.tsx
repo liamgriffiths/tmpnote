b5e36c1ed2610fc5244b41b9de18e5a2aa2302e9
Liam Griffiths <liamgriffiths@gmail.com>
2022-06-23 10:10:15 -0400

    Update start page

--- components/Start.tsx
+++ components/Start.tsx
@@ -1,20 +1,33 @@
 import React from "react";
+import Link from "next/link";
 
 type Props = {
   onStart: () => void;
 };
 
 const Start = (props: Props) => {
+  const TweetNaclLink = () => (
+    <span className="underline">
+      <Link href="https://tweetnacl.js.org/">TweetNaCL.js</Link>
+    </span>
+  );
+
+  const GitHubLink = () => (
+    <span className="underline">
+      <Link href="https://github.com/liamgriffiths/tmpnote">GitHub</Link>
+    </span>
+  );
+
   return (
     <div className="flex flex-col items-center">
       <h1 className="font-thin text-5xl m-4">Create & Share Encrypted Notes</h1>
 
-      <div className="text-center m-10">
-        <pre>Self-destructs when read.</pre>
-        <pre>Self-destructs in 7 days.</pre>
-        <pre>Encrypted withTweetNaCL.js.</pre>
-        <pre>No trackers or ads.</pre>
-        <pre>Open-source on GitHub.</pre>
+      <div className="text-center m-10 font-mono">
+        <div>Self-destructs when read.</div>
+        <div>Self-destructs in 7 days.</div>
+        <div>Encrypted with <TweetNaclLink />.</div>
+        <div>No trackers or ads.</div>
+        <div> Open-source on <GitHubLink />.</div>
       </div>
 
       <button
