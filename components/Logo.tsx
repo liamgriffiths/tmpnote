41e8e273624254cdd31877ccd27251b5d5bf824a
Liam Griffiths <liamgriffiths@gmail.com>
2022-06-22 23:03:47 -0400

    Use next/link

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,13 +1,11 @@
 import React from "react";
+import Link from "next/link";
 
 const Logo = () => {
   return (
-    <a
-      href="/"
-      className="skew-y-2 font-black text-5xl center text-black-500 hover:text-blue-600 drop-shadow-2xl"
-    >
-      tmpnote
-    </a>
+    <span className="skew-y-2 font-black text-5xl center text-black-500 hover:text-blue-600 drop-shadow-2xl">
+      <Link href="/">tmpnote</Link>
+    </span>
   );
 };
 
